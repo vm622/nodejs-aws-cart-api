66e5cab0d17bfc735c75067bafa26c09c832e40b
vm622 <vm62285@gmail.com>
2024-08-02 04:09:26 +0300

    integrate rds with lambda

--- aws-deploy/aws_deploy/aws_deploy_stack.py
+++ aws-deploy/aws_deploy/aws_deploy_stack.py
@@ -10,29 +10,30 @@ from aws_cdk import (
     CfnOutput
 )
 from constructs import Construct
+import os
+from dotenv import load_dotenv
 
 class AwsDeployStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
+        load_dotenv()
 
         cart_service_vpc = ec2.Vpc(
             self, "CartServiceVpc",
-            ip_addresses= ec2.IpAddresses.cidr("10.0.0.0/16"),
             max_azs= 2,
-            subnet_configuration=[
-                ec2.SubnetConfiguration(
-                    name="CartServiceRdsSub", cidr_mask=24, subnet_type=ec2.SubnetType.PRIVATE_ISOLATED
-                ),
-                ec2.SubnetConfiguration(
-                    name="CartServiceLmbdaSub", cidr_mask=24, subnet_type=ec2.SubnetType.PUBLIC
-                ),
-            ]
         )
 
-        cart_service_rds_sg = ec2.SecurityGroup(self, "CartServiceRdsSg", vpc=cart_service_vpc, allow_all_outbound=True)
-        cart_service_function_sg = ec2.SecurityGroup(self, "CartServiceLambdaTest", vpc=cart_service_vpc, allow_all_outbound=True)
+        cart_service_rds_sg = ec2.SecurityGroup(self, "CartServiceRdsSg", vpc=cart_service_vpc)
+        cart_service_function_sg = ec2.SecurityGroup(self, "CartServiceLambdaTest", vpc=cart_service_vpc)
+
+        cart_lambda_env = {
+            "RDS_ENDPOINT": os.getenv('RDS_ENDPOINT'),
+            "RDS_PORT": os.getenv('RDS_PORT'),
+            "RDS_DATABASE": os.getenv('RDS_DATABASE'),
+            "RDS_USERNAME": os.getenv('RDS_USERNAME'),
+            "RDS_PASSWORD": os.getenv('RDS_PASSWORD'),
+        }  
 
         cart_service_function = _lambda.Function(
             self,
@@ -42,7 +43,11 @@ class AwsDeployStack(Stack):
             code = _lambda.Code.from_asset("../dist"),
             handler = "main.handler", 
             memory_size = 1024,
-            timeout=Duration.seconds(30)
+            timeout=Duration.seconds(30),
+            vpc=cart_service_vpc,
+            vpc_subnets=ec2.SubnetSelection(subnets=cart_service_vpc.private_subnets),
+            security_groups=[cart_service_function_sg],
+            environment=cart_lambda_env
         )
 
         apigw.LambdaRestApi(
@@ -73,9 +78,7 @@ class AwsDeployStack(Stack):
             database_name="CartDatabase",
             engine= rds.DatabaseInstanceEngine.POSTGRES,
             vpc= cart_service_vpc,
-            vpc_subnets= ec2.SubnetSelection(
-                subnet_type= ec2.SubnetType.PUBLIC,
-            ),
+            vpc_subnets=ec2.SubnetSelection(subnets=cart_service_vpc.private_subnets),
             credentials= rds_credentials,
             security_groups=[cart_service_rds_sg],
             instance_type= ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
@@ -83,13 +86,10 @@ class AwsDeployStack(Stack):
             allocated_storage= 80,
             multi_az= False,
             removal_policy= RemovalPolicy.DESTROY,
-            publicly_accessible= True
+            publicly_accessible= False
         )
-
-        cart_service_rds.connections.allow_from_any_ipv4(
-            ec2.Port.tcp(5432),
-            description= "Open port for connection"
-        )
+        
+        cart_service_rds.connections.allow_from(cart_service_function_sg, ec2.Port.tcp(5432))
 
         CfnOutput(self, "CartServiceRdsEndpointAddress", value= cart_service_rds.db_instance_endpoint_address)
         CfnOutput(self, "CartServiceRdsSecretArn", value=rds_secret.secret_arn)
